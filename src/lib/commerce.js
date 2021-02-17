437a9919871f3f86d4d506f59617f528687b6e16
Nathan Kima Yuave <natekyuave@gmail.com>
2021-02-18 10:31:54 +1100

    changed api key name

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,2 +1,2 @@
 import Commerce from '@chec/commerce.js';
-export const commerce = new Commerce(process.env.REACT_APP_leafMART_KEY, true);
+export const commerce = new Commerce(process.env.REACT_APP_LEAF_MART_KEY, true);
